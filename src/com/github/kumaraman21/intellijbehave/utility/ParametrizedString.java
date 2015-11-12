12e253a52fc6a935e059df9bebbe6044684ed729
datensenke <dinis@chello.at>
2015-11-12 11:55:13 +0100

    removed WeightChain

--- src/com/github/kumaraman21/intellijbehave/utility/ParametrizedString.java
+++ src/com/github/kumaraman21/intellijbehave/utility/ParametrizedString.java
@@ -109,105 +109,6 @@ public class ParametrizedString implements Comparable<ParametrizedString> {
         return toStringWithoutIdentifiers().compareTo(other.toStringWithoutIdentifiers());
     }
 
-    public Token getToken(int index) {
-        return tokens.get(index);
-    }
-
-    private WeightChain calculateWeightChain(String input) {
-        WeightChain chain = acceptsBeginning(0, input, 0);
-        chain.input = input;
-        chain.collectWeights();
-        return chain;
-    }
-
-    public List<StringToken> tokenize(String stepInput) {
-
-        List<StringToken> stringTokens = new ArrayList<StringToken>();
-
-        WeightChain chain = calculateWeightChain(stepInput);
-        List<String> inputTokens = chain.tokenize();
-        while (chain != null) {
-            if (!chain.isZero()) {
-                Token token = tokens.get(chain.getTokenIndex());
-                String value = inputTokens.get(chain.getTokenIndex());
-                stringTokens.add(new StringToken(value, token.isIdentifier()));
-            }
-            chain = chain.getNext();
-        }
-        return stringTokens;
-    }
-
-    private WeightChain acceptsBeginning(int inputIndex, String input, int tokenIndexStart) {
-        WeightChain pair = new WeightChain();
-        pair.inputIndex = inputIndex;
-
-        WeightChain current = pair;
-
-        List<Token> tokenList = this.tokens;
-        for (int tokenIndex = tokenIndexStart, n = tokenList.size(); tokenIndex < n; tokenIndex++) {
-            boolean isLastToken = (tokenIndex == n - 1);
-            Token token = tokenList.get(tokenIndex);
-            if (!token.isIdentifier()) {
-                int remaining = input.length() - inputIndex;
-                if (remaining > token.getLength() && isLastToken) {
-                    // more data than the token itself
-                    return WeightChain.zero();
-                }
-
-                int overlaping = Math.min(token.getLength(), remaining);
-                if (overlaping > 0) {
-                    if (token.regionMatches(0, input, inputIndex, overlaping)) {
-                        current.tokenIndex = tokenIndex;
-                        current.weight++;
-                        if (overlaping == token.getLength()) // full token match
-                        {
-                            current.weight++;
-                            if ((inputIndex + overlaping) == input.length())
-                            // no more data, break the loop now
-                            {
-                                return pair;
-                            }
-                        } // break looop
-                        else {
-                            return pair;
-                        }
-
-                        inputIndex += overlaping;
-                        WeightChain next = new WeightChain();
-                        next.inputIndex = inputIndex;
-                        current.next = next;
-                        current = next;
-                    } else {
-                        // no match
-                        return WeightChain.zero();
-                    }
-                } else {
-                    // not enough data, returns what has been collected
-                    return pair;
-                }
-            } else {
-                current.tokenIndex = tokenIndex;
-                current.weight++;
-
-                // not the most efficient part, but no other solution right now
-                WeightChain next = WeightChain.zero();
-                for (int j = inputIndex + 1; j < input.length(); j++) {
-                    WeightChain sub = acceptsBeginning(j, input, tokenIndex + 1);
-                    if (sub.hasMoreWeightThan(next)) {
-                        next = sub;
-                    }
-                }
-                current.next = next;
-                return pair;
-            }
-        }
-        return pair;
-    }
-
-    public boolean isSameAs(String input) {
-        return complete(input, false).equals(input);
-    }
-
     public String complete(String input, boolean hasPostParameter) {
         List<ContentToken> builder = new ArrayList<ContentToken>();
         List<StringToken> myTokens = new ArrayList<StringToken>();
@@ -483,84 +384,6 @@ public class ParametrizedString implements Comparable<ParametrizedString> {
         }
     }
 
-    public static class WeightChain {
-        private String input;
-        private int inputIndex;
-        private int weight;
-        private int tokenIndex = -1;
-        private WeightChain next;
-
-        public static WeightChain zero() {
-            return new WeightChain();
-        }
-
-        public WeightChain last() {
-            WeightChain last = this;
-            WeightChain iter = this;
-            while (iter != null) {
-                if (!iter.isZero()) {
-                    last = iter;
-                }
-                iter = iter.next;
-            }
-            return last;
-        }
-
-        public boolean isZero() {
-            return weight == 0 && tokenIndex == -1;
-        }
-
-        public WeightChain getNext() {
-            return next;
-        }
-
-        public int getTokenIndex() {
-            return tokenIndex;
-        }
-
-        public boolean hasMoreWeightThan(WeightChain pair) {
-            return weight > pair.weight;
-        }
-
-        @Override
-        public String toString() {
-            return "WeightChain [inputIndex=" + inputIndex + ", weight=" + weight + ", tokenIndex=" + tokenIndex + "]";
-        }
-
-        public void collectWeights() {
-            int w = weight;
-            WeightChain n = next;
-            while (n != null) {
-                if (!n.isZero()) {
-                    w += n.weight;
-                }
-                n = n.next;
-            }
-
-            this.weight = w;
-        }
-
-        public List<String> tokenize() {
-            List<String> parts = new ArrayList<String>();
-            if (isZero()) {
-                return parts;
-            }
-
-            int indexBeg = inputIndex;
-            WeightChain n = next;
-            while (n != null) {
-                if (!n.isZero()) {
-                    parts.add(input.substring(indexBeg, n.inputIndex));
-                    indexBeg = n.inputIndex;
-                }
-                n = n.next;
-            }
-            parts.add(input.substring(indexBeg));
-
-            return parts;
-        }
-    }
-
     public class Token {
         private final int offset;
         private final int length;
@@ -635,14 +458,6 @@ public class ParametrizedString implements Comparable<ParametrizedString> {
                 start = 0;
                 end = 0;
             }
-        }
-
-        public String getContent() {
-            return content;
-        }
-
-        public int getEnd() {
-            return end;
         }
 
         public int getStart() {
