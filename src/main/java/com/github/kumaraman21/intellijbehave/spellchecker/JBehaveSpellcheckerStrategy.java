304baa378fd9c0c874dd9bb1d52c12053c8ea8da
Catalin Ciobanu <caciobanu@gmail.com>
2021-08-07 17:58:12 +0300

    Support for IntelliJ IDEA 2021.2

--- src/main/java/com/github/kumaraman21/intellijbehave/spellchecker/JBehaveSpellcheckerStrategy.java
+++ src/main/java/com/github/kumaraman21/intellijbehave/spellchecker/JBehaveSpellcheckerStrategy.java
@@ -2,12 +2,8 @@ package com.github.kumaraman21.intellijbehave.spellchecker;
 
 import com.github.kumaraman21.intellijbehave.parser.StoryElementType;
 import com.intellij.lang.ASTNode;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafElement;
-import com.intellij.spellchecker.quickfixes.AcceptWordAsCorrect;
-import com.intellij.spellchecker.quickfixes.ChangeTo;
-import com.intellij.spellchecker.quickfixes.SpellCheckerQuickFix;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import org.jetbrains.annotations.NotNull;
@@ -23,9 +19,5 @@ public class JBehaveSpellcheckerStrategy extends SpellcheckingStrategy {
         }
 
         return super.getTokenizer(element);
-    }
-
-    public SpellCheckerQuickFix[] getRegularFixes(PsiElement element, int offset, @NotNull TextRange textRange, boolean useRename, String wordWithTypo) {
-        return new SpellCheckerQuickFix[]{new ChangeTo(wordWithTypo), new AcceptWordAsCorrect(wordWithTypo)};
     }
 }
